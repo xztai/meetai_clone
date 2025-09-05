06b9fc89916e61938a87cec8746c52e14f62d4ff
tai <nah>
2025-09-05 12:01:46 +0500

    04: authentication ui

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">{children}</div>
+    </div>
+  )
+}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/db";
-import * as schema from "@/db/schema";
+import { betterAuth } from "better-auth"
+import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { db } from "@/db"
+import * as schema from "@/db/schema"
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -13,4 +13,4 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-});
+})
